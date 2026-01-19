13788226012fd334c149ddffcda17441020c11e8
bluesn0on <namuka321@outlook.com>
2026-01-19 21:47:24 +0800

    update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let maintext = document.getElementById('maintext');
+let maintext1 = document.getElementById('maintext1');
+let maintext2 = document.getElementById('maintext2');
 let grass = document.getElementById('grass');
 let city1 = document.getElementById('city1');
 let city2 = document.getElementById('city2');
@@ -10,5 +11,12 @@ let skybox = document.getElementById('skybox');
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
-    maintext.style.marginTop = value * 2.5 + 'px';
+    maintext1.style.marginTop = value * 1.5 + 'px';
+    maintext2.style.top = value * 1 + 'px';
+    grass.style.top = value * -0.3 + 'px';
+    city1.style.top = value * -0.2 + 'px';
+    city2.style.top = value * -0.3 + 'px';
+    city3.style.top = value * -0.2 + 'px';
+    cloud1.style.left = value * -0.5 + 'px';
+    cloud2.style.left = value * 0.5 + 'px';
 });
