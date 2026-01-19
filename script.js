b4e29f455b390961ac4d4b7a0da2d767a278caf2
bluesn0on <namuka321@outlook.com>
2026-01-19 23:58:57 +0800

    update

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let maintext1 = document.getElementById('maintext1');
 let maintext2 = document.getElementById('maintext2');
-let grass = document.getElementById('grass');
 let city1 = document.getElementById('city1');
 let city2 = document.getElementById('city2');
 let city3 = document.getElementById('city3');
@@ -12,10 +11,9 @@ window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
     maintext1.style.marginTop = value * 1.5 + 'px';
-    maintext2.style.top = value * 1 + 'px';
-    grass.style.top = value * -0.3 + 'px';
-    city1.style.top = value * -0.2 + 'px';
-    city2.style.top = value * -0.3 + 'px';
+    maintext2.style.top = value * 0.5 + 'px';
+    city1.style.top = value * -0.3 + 'px';
+    city2.style.top = value * -0.2 + 'px';
     city3.style.top = value * -0.2 + 'px';
     cloud1.style.left = value * -0.5 + 'px';
     cloud2.style.left = value * 0.5 + 'px';
